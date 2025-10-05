7642a5d80d8ff3ac152c1c6f89b23d46be6d12b1
S4m0912 <saamuel.quintero@gmail.com>
2025-10-05 18:04:49 -0300

    Agrego la clase DisplayElement que se va a encargar de mostrar las diferentes skins de nuestros elementos

---
+++ gameOfLife/src/main/java/DisplayElement.java
@@ -0,0 +1,4 @@
+public interface DisplayElement { //de aqui van a heredar las diferentes vistas (skins)
+                                //que le queramos meter a nuestro board 
+	public void display();
+}
